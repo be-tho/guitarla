777643a526a5652fe00e09a305279efdfc633590
albert <yiyevp@gmail.com>
2024-09-15 20:27:58 -0300

    state derivado de carritoEmpty

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,7 @@
-export default function Header() {
+export default function Header({cart}) {
+
+    //state derivado
+    const isEmpyCart = () => cart.length === 0;
   return (
     <>
     <header className="py-5 header">
@@ -16,7 +19,9 @@ export default function Header() {
                         <img className="img-fluid" src="./public/img/carrito.png" alt="imagen carrito" />
 
                         <div id="carrito" className="bg-white p-3">
-                            <p className="text-center">El carrito esta vacio</p>
+                            { isEmpyCart() ?  ( 
+                                <p className="text-center">El carrito esta vacio</p>
+                            ) : (
                             <table className="w-100 table">
                                 <thead>
                                     <tr>
@@ -28,13 +33,17 @@ export default function Header() {
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
+                                    { cart.map(guitar => (
+                                    <tr key={guitar.key}>
                                         <td>
-                                            <img className="img-fluid" src="./public/img/guitarra_02.jpg" alt="imagen guitarra" />
+                                            <img 
+                                            className="img-fluid" 
+                                            src={`/img/${guitar.image}.jpg`}
+                                            alt="imagen guitarra" />
                                         </td>
-                                        <td>SRV</td>
+                                        <td>{guitar.name}</td>
                                         <td className="fw-bold">
-                                                $299
+                                                ${guitar.price}
                                         </td>
                                         <td className="flex align-items-start gap-4">
                                             <button
@@ -43,7 +52,7 @@ export default function Header() {
                                             >
                                                 -
                                             </button>
-                                                1
+                                                {guitar.quantity}
                                             <button
                                                 type="button"
                                                 className="btn btn-dark"
@@ -60,9 +69,10 @@ export default function Header() {
                                             </button>
                                         </td>
                                     </tr>
+                                    ))}
                                 </tbody>
                             </table>
-
+                            )}
                             <p className="text-end">Total pagar: <span className="fw-bold">$899</span></p>
                             <button className="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
                         </div>
