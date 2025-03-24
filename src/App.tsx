b30b245d84322af8a830098ddd0fc0fe5aa346df
Albert Villarroel <56642267+be-tho@users.noreply.github.com>
2025-03-23 21:16:02 -0300

    migrar al reducer add-to-cart

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { useReducer } from "react"
 import Guitar from "./components/Guitar"
 import Header from "./components/Header"
 import { useCart } from './hooks/useCart'
+import { cartReducer, inialState } from "./reducers/cart-reducer"
 
 function App() {
 
-  const { data, cart, addToCart, removeFromCart, decreaseQuantity, increaseQuantity, clearCart, isEmpty, cartTotal } = useCart()
+  const { cart, removeFromCart, decreaseQuantity, increaseQuantity, clearCart, isEmpty, cartTotal } = useCart()
+  const [state, dispatch] = useReducer(cartReducer, inialState)
 
   return (
     <>
@@ -22,11 +25,11 @@ function App() {
           <h2 className="text-center">Nuestra Colección</h2>
 
           <div className="row mt-5">
-              {data.map((guitar) => (
+              {state.data.map((guitar) => (
                   <Guitar 
                     key={guitar.id}
                     guitar={guitar}
-                    addToCart={addToCart}
+                    dispatch={dispatch}
                   />
               ))}
               
---
+++ src/reducers/cart-reducer.ts
@@ -0,0 +1,76 @@
+import { db } from '../data/db'
+import type { Guitar, CartItem } from '../types'
+
+export type CartActions = 
+    {type : 'add-to-cart', payload : { item : Guitar}} |
+    {type : 'remove-from-cart', payload : { id : Guitar['id']}} |
+    {type : 'decrease-quantity', payload : { id : Guitar['id']}} |
+    {type : 'increase-quantity', payload : { id : Guitar['id']}} |
+    {type : 'clear-cart'}
+    
+
+export type CartState = {
+    data : Guitar[]
+    cart : CartItem[]
+}
+
+export const inialState : CartState = {
+    data: db,
+    cart: []
+}
+
+const MIN_ITEMS = 1
+const MAX_ITEMS = 5
+
+export const cartReducer = (
+        state : CartState = inialState,
+        action : CartActions
+    )=> {
+
+    //agrega un item al carrito
+    if(action.type === 'add-to-cart'){
+
+        const itemExists = state.cart.find(guitar => guitar.id === action.payload.item.id)
+
+        let updatedCart : CartItem[] = []
+
+        if(itemExists) { // existe en el carrito
+            updatedCart = state.cart.map(item => {
+                if(item.id === action.payload.item.id){
+                    if(item.quantity >= MAX_ITEMS) {
+                    return {...item, quantity : item.quantity + 1}
+                    }else{
+                        return item
+                    }
+                }else{
+                    return item
+                }
+           })
+        } else {
+            const newItem : CartItem = {...action.payload.item, quantity : 1}
+            updatedCart = [...state.cart, newItem]
+        }
+        return {
+            ...state,
+            cart : updatedCart
+        }
+    }
+
+    //elimina un item del carrito
+    if(action.type === 'remove-from-cart'){
+        return {
+            ...state,
+            cart : state.cart.filter(item => item.id !== action.payload.id)
+        }
+    }
+
+    
+    if(action.type === 'decrease-quantity'){
+        return {
+            ...state,
+            
+        }
+    }
+
+    return state
+}
