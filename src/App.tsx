566fca379cc3e34b02539f1f93b651d18cc4109a
Albert Villarroel <56642267+be-tho@users.noreply.github.com>
2025-03-24 00:21:23 -0300

    local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,46 @@
-import { useReducer} from "react"
+import { useReducer, useEffect} from "react"
 import Guitar from "./components/Guitar"
 import Header from "./components/Header"
 import { cartReducer, inialState } from "./reducers/cart-reducer"
 
 function App() {
 
-  const [state, dispatch] = useReducer(cartReducer, inialState)
+  	const [state, dispatch] = useReducer(cartReducer, inialState)
 
-  return (
-    <>
-      <Header 
-        cart={state.cart}
-        dispatch={dispatch}
-      />
-      
-      <main className="container-xl mt-5">
-          <h2 className="text-center">Nuestra Colección</h2>
+  	useEffect(() => {
+    	localStorage.setItem('cart', JSON.stringify(state.cart))
+  	}, [state.cart])
 
-          <div className="row mt-5">
-              {state.data.map((guitar) => (
-                  <Guitar 
-                    key={guitar.id}
-                    guitar={guitar}
-                    dispatch={dispatch}
-                  />
-              ))}
-              
-          </div>
-      </main>
+  	return (
+		<>
+		<Header 
+			cart={state.cart}
+			dispatch={dispatch}
+		/>
+		
+		<main className="container-xl mt-5">
+			<h2 className="text-center">Nuestra Colección</h2>
+
+			<div className="row mt-5">
+				{state.data.map((guitar) => (
+					<Guitar 
+						key={guitar.id}
+						guitar={guitar}
+						dispatch={dispatch}
+					/>
+				))}
+				
+			</div>
+		</main>
 
 
-      <footer className="bg-dark mt-5 py-5">
-          <div className="container-xl">
-              <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
-          </div>
-      </footer>
-    </>
-  )
+		<footer className="bg-dark mt-5 py-5">
+			<div className="container-xl">
+				<p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+			</div>
+		</footer>
+		</>
+  	)
 }
 
 export default App
--- src/reducers/cart-reducer.ts
+++ src/reducers/cart-reducer.ts
@@ -14,9 +14,14 @@ export type CartState = {
     cart : CartItem[]
 }
 
+const initialCart = () : CartItem[] => {
+    const localStorageCart = localStorage.getItem('cart')
+    return localStorageCart ? JSON.parse(localStorageCart) : []
+}
+
 export const inialState : CartState = {
     data: db,
-    cart: []
+    cart: initialCart()
 }
 
 const MIN_ITEMS = 1
